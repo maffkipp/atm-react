4b7edc00b9e65bb44b4ef7753ced71ac4d8d81f0
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2017-10-26 19:54:08 -0500

    Wes MK completed hw assignment

--- src/Account.js
+++ src/Account.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
 
 export default class Account extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      balance: 0
+    };
+    this.deposit = this.deposit.bind(this);
+    this.withdraw = this.withdraw.bind(this);
+  }
+
+  deposit() {
+    let depositAmount = parseInt(this.inputBox.value, 10);
+    this.setState({
+      balance: this.state.balance + depositAmount
+    });
+  }
+
+  withdraw() {
+    let withdrawalAmount = parseInt(this.inputBox.value, 10);
+    if (withdrawalAmount <= this.state.balance) {
+      this.setState({
+        balance: this.state.balance - withdrawalAmount
+      });
+    } else {
+      this.setState({
+        balance: 0
+      });
+    }
+  }
+
   render() {
+    let balanceClass = 'balance';
+    if (this.state.balance === 0) {
+      balanceClass += ' zero';
+    }
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className={balanceClass}>${this.state.balance}</div>
+        <input type="text" placeholder="enter an amount" ref={(input)=>this.inputBox=input} />
+        <input type="button" value="Deposit" onClick={this.deposit}/>
+        <input type="button" value="Withdraw" onClick={this.withdraw} />
       </div>
     )
   }
